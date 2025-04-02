12a8c6399055fa644662cf4279d857a594558815
Larry_Tomson <65211694+Larry-Tomson@users.noreply.github.com>
2025-04-02 15:07:22 +0900

    add presistense to entities

--- src/main/java/com/luhtom/expensetracker/entity/Category.java
+++ src/main/java/com/luhtom/expensetracker/entity/Category.java
@@ -2,14 +2,28 @@ package com.luhtom.expensetracker.entity;
 
 import java.util.List;
 
+import org.springframework.stereotype.Indexed;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+
     private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Expense> expenses;
 }
--- src/main/java/com/luhtom/expensetracker/entity/Expense.java
+++ src/main/java/com/luhtom/expensetracker/entity/Expense.java
@@ -3,15 +3,35 @@ package com.luhtom.expensetracker.entity;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false)
     private Long id;
+
+    @Column(nullable = false)
     private String description;
+
+    @Column(nullable = false)
     private BigDecimal amount;
+
+    @Column(nullable = false)
     private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 }
